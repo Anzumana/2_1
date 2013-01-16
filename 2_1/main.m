f9ea3b8f33fc182f0649b5b46c695d0dabc69256
Anzumana <Anzumana.Taal@me.com>
2013-01-16 15:11:04 +0100

    prog 2.4

--- 2_1/main.m
+++ 2_1/main.m
@@ -12,8 +12,10 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
-        
-        NSLog (@"...\n..1\n...2\n....3\n");
+      
+        int sum;
+        sum = 50 + 25;
+        NSLog (@"The sum of 50 and 25 is %i",sum);
     }
     return 0;
 }

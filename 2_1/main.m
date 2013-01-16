ab7c047359ee479cb02340534052038a5b849dbd
Anzumana <Anzumana.Taal@me.com>
2013-01-16 15:02:47 +0100

    second nslprint statement added

--- 2_1/main.m
+++ 2_1/main.m
@@ -13,9 +13,9 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
-        // insert code here...
+      
         NSLog(@"Programming is fun!");
-        
+        NSLog (@"Programming in Objective-C is even more fun!");
     }
     return 0;
 }

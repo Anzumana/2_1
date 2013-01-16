fc9edee66712829b6437fa79490c5643133d255a
Anzumana <Anzumana.Taal@me.com>
2013-01-16 16:54:57 +0100

    3_2

--- 2_1/main.m
+++ 2_1/main.m
@@ -5,18 +5,63 @@
 //  Created by Anzumana Taal on 16.01.13.
 //  Copyright (c) 2013 Anzumana Taal. All rights reserved.
 //
+// Program to work with fractions - class version
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[])
+//---- @interface section ----
+
+@interface Fraction:NSObject
+
+-(void) print;
+-(void) setNumerator: (int) n;
+-(void) setDenominator: (int) d;
+
+@end
+
+@implementation Fraction
 {
+	int numerator;
+	int denominator;
+}
+-(void) print
+{
+	NSLog(@"%i/%i",numerator , denominator);
+}
 
-    @autoreleasepool {
-		int numerator = 1;
-		int denominator = 3;
-		NSLog (@"The fraction is %i/%i", numerator , denominator);
+-(void) setNumerator: (int) n
+{
+	numerator = n;
+}
 
-    }
+-(void) setDenominator: (int) d
+{
+	denominator = d;
+}
+
+@end
+//---- program section ----
+
+int main (int argc, char * argv[])
+{
+	@autoreleasepool {
+		Fraction *myFraction;
+
+		// Create an instance of a Fraction 
+
+		myFraction = [Fraction alloc];
+		myFraction = [myFraction init];
+
+		// Set fraction to 1/3
+
+		[myFraction setNumerator:1];
+		[myFraction setDenominator:3];
+
+		// Display the fraction using the print methog 
+
+		NSLog (@"The value of myFraction is :");
+		[myFraction print];
+}
     return 0;
 }
 

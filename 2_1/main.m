d4441befaffa8ddbaa1ecd2dceb4b4db3eb24461
Anzumana <Anzumana.Taal@me.com>
2013-01-16 14:37:55 +0100

    Initial Commit

---
+++ 2_1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  2_1
+//
+//  Created by Anzumana Taal on 16.01.13.
+//  Copyright (c) 2013 Anzumana Taal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
